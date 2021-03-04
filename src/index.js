e2ad6686710d09112a7e6bb5965312b8e92392b3
ASlawinska <slawinska.adrianna@gmail.com>
2021-03-04 16:38:04 +0100

    Added hamburger menu for mobile and tablet version

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import style from "./scss/index.scss";
 //Burger menu
-// const burgerBtn = document.querySelector('.burger');
-// const open = document.querySelector('.fa-bars');
-// const close = document.querySelector('.fa-times');
-// const nav = document.querySelector('.nav')
+const burgerBtn = document.querySelector('.burger');
+const open = document.querySelector('.fa-bars');
+const close = document.querySelector('.fa-times');
+const nav = document.querySelector('.nav')
 
-// const addActive = () => {
-//     nav.classList.toggle('active');
-//     burgerBtn.classList.toggle('active');
-//     if (burgerBtn.classList.contains('active')) {
-//         open.classList.add('hide');
-//         close.classList.remove('hide');
-//     } else {
-//         open.classList.remove('hide');
-//         close.classList.add('hide');
-//     }
-// }
-// burgerBtn.addEventListener('click', addActive)
+const addActive = () => {
+    nav.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+    if (burgerBtn.classList.contains('active')) {
+        open.classList.add('hide');
+        close.classList.remove('hide');
+    } else {
+        open.classList.remove('hide');
+        close.classList.add('hide');
+    }
+}
+burgerBtn.addEventListener('click', addActive)
 
 
 // Year footer
