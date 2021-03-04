97741cca597176c231da2a8f06d8d331cd549be6
ASlawinska <slawinska.adrianna@gmail.com>
2021-03-04 15:16:36 +0100

    edited code of header section, styling of navigation, added variables and mixins

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import style from "./scss/index.scss";
+//Burger menu
+// const burgerBtn = document.querySelector('.burger');
+// const open = document.querySelector('.fa-bars');
+// const close = document.querySelector('.fa-times');
+// const nav = document.querySelector('.nav')
 
-console.log("Hello world");
+// const addActive = () => {
+//     nav.classList.toggle('active');
+//     burgerBtn.classList.toggle('active');
+//     if (burgerBtn.classList.contains('active')) {
+//         open.classList.add('hide');
+//         close.classList.remove('hide');
+//     } else {
+//         open.classList.remove('hide');
+//         close.classList.add('hide');
+//     }
+// }
+// burgerBtn.addEventListener('click', addActive)
+
+
+// Year footer
+let today = new Date();
+let year = today.getFullYear();
+let footer = document.querySelector('.footer');
+footer.innerHTML = `Copyright &copy; ${year} <br>AFM diploma thesis Adrianna Sławińska`
