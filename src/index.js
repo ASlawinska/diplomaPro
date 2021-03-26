ac6f62614437538f4e82f143160ad1fa3c92b42d
ASlawinska <slawinska.adrianna@gmail.com>
2021-03-26 20:13:26 +0100

    Added svg file and js code contain the date format

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import style from "./scss/index.scss";
+
 //Burger menu
 const burgerBtn = document.querySelector('.burger');
 const open = document.querySelector('.fa-bars');
@@ -17,9 +18,31 @@ const addActive = () => {
 }
 burgerBtn.addEventListener('click', addActive)
 
+// Date form 
+const thisDay = document.getElementById('today');
+const tommorow = document.getElementById('tommorow');
+let today = new Date();
+let day = today.getDate() <10 ? `0${today.getDate()}`: `${today.getDate()}`;
+let dayTommorow = (today.getDate()+1) <10 ? `0${today.getDate()+1}`: `${today.getDate()+1}`;
+let month = (today.getMonth()+1) <10 ? `0${(today.getMonth()+1)}`: `${(today.getMonth()+1)}`;
+let year = today.getFullYear();
+
+
+let dateFormat = `${day}-${month}-${year}`;
+let dateFormat2 = `${dayTommorow}-${month}-${year}`;
+let todayMin = thisDay.setAttribute('min', dateFormat);
+let tommorowMin = tommorow.setAttribute('min', dateFormat2);
+thisDay.setAttribute('value', dateFormat2);
+
+
+console.log(day);
+console.log(month);
+console.log(year);
+console.log(dateFormat);
+console.log(dateFormat2);
+
 
 // Year footer
-let today = new Date();
-let year = today.getFullYear();
+// let year = today.getFullYear();
 let footer = document.querySelector('.footer');
 footer.innerHTML = `Copyright &copy; ${year} <br>AFM diploma thesis Adrianna Sławińska`
