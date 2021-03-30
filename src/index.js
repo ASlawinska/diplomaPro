24251ae9dfef5000db503b0b11be14640381baa5
ASlawinska <slawinska.adrianna@gmail.com>
2021-03-31 01:29:10 +0200

    Edited the date JS code - added tommorowDay functionality

--- src/index.js
+++ src/index.js
@@ -22,85 +22,89 @@ burgerBtn.addEventListener('click', addActive)
 const thisDay = document.getElementById('today');
 const tommorow = document.getElementById('tommorow');
 let today = new Date();
+let tommorowDay = new Date(today);
+tommorowDay.setDate(tommorowDay.getDate()+1);
+
 let day = today.getDate() <10 ? `0${today.getDate()}`: `${today.getDate()}`;
-let dayTommorow = (today.getDate()+1) <10 ? `0${today.getDate()+1}`: `${today.getDate()+1}`;
 let month = (today.getMonth()+1) <10 ? `0${(today.getMonth()+1)}`: `${(today.getMonth()+1)}`;
 let year = today.getFullYear();
-
+let dayTommorow = (tommorowDay.getDate()) <10 ? `0${tommorowDay.getDate()}`: `${tommorowDay.getDate()}`;
+let monthTommmorow = (tommorowDay.getMonth()+1) <10 ? `0${tommorowDay.getMonth()+1}`: `${(tommorowDay.getMonth()+1)}`;
+let yearTommorow = tommorowDay.getFullYear();
 
 let dateFormat = `${year}-${month}-${day}`;
-let dateFormat2 = `${year}-${month}-${dayTommorow}`;
+let dateFormat2 = `${yearTommorow}-${monthTommmorow}-${dayTommorow}`;
 let todayMin = thisDay.setAttribute('min', dateFormat);
 let tommorowMin = tommorow.setAttribute('min', dateFormat2);
 thisDay.setAttribute('value', dateFormat);
 tommorow.setAttribute('value', dateFormat2);
 
 //Weather APP
-const input = document.querySelector('input');
-const btn = document.querySelector('button');
+// const input = document.querySelector('input');
+// const btn = document.querySelector('button');
 
-const cityName = document.querySelector('.city-name');
-const warning = document.querySelector('.warning');
-const photo = document.querySelector('.photo');
+// const cityName = document.querySelector('.city-name');
+// const warning = document.querySelector('.warning');
+// const photo = document.querySelector('.photo');
 
-const weather = document.querySelector('.weather');
-const temperature = document.querySelector('.temp');
-const humidity = document.querySelector('.humidity');
+// const weather = document.querySelector('.weather');
+// const temperature = document.querySelector('.temp');
+// const humidity = document.querySelector('.humidity');
 
-const apilink = 'https://api.openweathermap.org/data/2.5/weather?q=';
-const apiKey = '&appid=1063abb9b196175704486b6c818e5cc7';
-const units = '&units=metric';
+// const apilink = 'https://api.openweathermap.org/data/2.5/weather?q=';
+// const apiKey = '&appid=1063abb9b196175704486b6c818e5cc7';
+// const units = '&units=metric';
 
-let city;
-let url;
+// let city;
+// let url;
 
-const getWeather = () => {
-    city = (!input.value) ? 'Warsaw': input.value; 
-    url = apilink + city + apiKey + units;
+// const getWeather = () => {
+//     city = (!input.value) ? 'Warsaw': input.value; 
+//     url = apilink + city + apiKey + units;
 
-    axios.get(url)
-        .then(res => {
-            const temp = res.data.main.temp;
-            const hum = res.data.main.humidity;
-            const status = Object.assign({}, ...res.data.weather);
+//     axios.get(url)
+//         .then(res => {
+//             const temp = res.data.main.temp;
+//             const hum = res.data.main.humidity;
+//             const status = Object.assign({}, ...res.data.weather);
             
-            cityName.textContent = res.data.name;
-            weather.textContent = status.main;
-            temperature.textContent = Math.floor(temp) + '°C';
-            humidity.textContent = hum + '%';
+//             cityName.textContent = res.data.name;
+//             weather.textContent = status.main;
+//             temperature.textContent = Math.floor(temp) + '°C';
+//             humidity.textContent = hum + '%';
 
-            warning.textContent = '';
-            input.value = '';
+//             warning.textContent = '';
+//             input.value = '';
 
-            // if (status.id >= 200 && status.id < 300) {
-            //     photo.setAttribute('src', "../img/thunderstorm.png")
-            // } else if (status.id >= 300 && status.id < 400) {
-            //     photo.setAttribute('src', "../img/drizzle.png")
-            // } else if (status.id >= 500 && status.id < 600) {
-            //     photo.setAttribute('src', "../img/rain.png")
-            // } else if  (status.id >= 600 && status.id < 700) {
-            //     photo.setAttribute('src', "../img/ice.png")
-            // } else if (status.id >= 700 && status.id < 799) {
-            //     photo.setAttribute('src', "../img/fog.png")
-            // } else if (status.id==800) {
-            //     photo.setAttribute('src', "../img/sun.png")
-            // } else if (status.id > 800 && status.id < 900) {
-            //     photo.setAttribute('src', "../img/cloud.png")
-            // } else {
-            //     photo.setAttribute('src', "../img/unknown.png")
-            // }
-        })
-        .catch(()=>{warning.textContent ='Wpisz poprawną nazwę miasta'})
-    };
+//             // if (status.id >= 200 && status.id < 300) {
+//             //     photo.setAttribute('src', "../img/thunderstorm.png")
+//             // } else if (status.id >= 300 && status.id < 400) {
+//             //     photo.setAttribute('src', "../img/drizzle.png")
+//             // } else if (status.id >= 500 && status.id < 600) {
+//             //     photo.setAttribute('src', "../img/rain.png")
+//             // } else if  (status.id >= 600 && status.id < 700) {
+//             //     photo.setAttribute('src', "../img/ice.png")
+//             // } else if (status.id >= 700 && status.id < 799) {
+//             //     photo.setAttribute('src', "../img/fog.png")
+//             // } else if (status.id==800) {
+//             //     photo.setAttribute('src', "../img/sun.png")
+//             // } else if (status.id > 800 && status.id < 900) {
+//             //     photo.setAttribute('src', "../img/cloud.png")
+//             // } else {
+//             //     photo.setAttribute('src', "../img/unknown.png")
+//             // }
+//         })
+//         .catch(()=>{warning.textContent ='Wpisz poprawną nazwę miasta'})
+//     };
 
-const enterCheck = () => {
-    if (event.keyCode===13) {
-        getWeather();
-    }
-}
-getWeather();
-input.addEventListener('keyup', enterCheck)
-btn.addEventListener('click', getWeather);
+// const enterCheck = () => {
+//     if (event.keyCode===13) {
+//         getWeather();
+//     }
+// }
+// getWeather();
+// input.addEventListener('keyup', enterCheck)
+// btn.addEventListener('click', getWeather);
 
 // Year footer
 let footer = document.querySelector('.footer');
