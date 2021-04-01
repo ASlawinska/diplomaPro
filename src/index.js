dd3c150707d4c6000c7fc1ca5b3a3f72824a42df
ASlawinska <slawinska.adrianna@gmail.com>
2021-04-01 20:19:24 +0200

    Reedited  of weather app section

--- src/index.js
+++ src/index.js
@@ -39,72 +39,77 @@ let tommorowMin = tommorow.setAttribute('min', dateFormat2);
 thisDay.setAttribute('value', dateFormat);
 tommorow.setAttribute('value', dateFormat2);
 
-//Weather APP
-// const input = document.querySelector('input');
-// const btn = document.querySelector('button');
+// Weather APP
+const input = document.querySelector('#cityNameInput');
+const btn = document.querySelector('button');
 
-// const cityName = document.querySelector('.city-name');
-// const warning = document.querySelector('.warning');
-// const photo = document.querySelector('.photo');
+const cityName = document.querySelector('.city-name');
+const warning = document.querySelector('.warning');
+const photo = document.querySelector('.photo');
 
-// const weather = document.querySelector('.weather');
-// const temperature = document.querySelector('.temp');
-// const humidity = document.querySelector('.humidity');
+const pressure = document.querySelector('.pressure');
+const temperature = document.querySelector('.temp');
+const humidity = document.querySelector('.humidity');
 
-// const apilink = 'https://api.openweathermap.org/data/2.5/weather?q=';
-// const apiKey = '&appid=1063abb9b196175704486b6c818e5cc7';
-// const units = '&units=metric';
+const apilink = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&appid=1063abb9b196175704486b6c818e5cc7';
+const units = '&units=metric';
 
-// let city;
-// let url;
+let city;
+let url;
 
-// const getWeather = () => {
-//     city = (!input.value) ? 'Warsaw': input.value; 
-//     url = apilink + city + apiKey + units;
+const getWeather = () => {
+    city = (!input.value) ? 'Warsaw': input.value; 
+    url = apilink + city + apiKey + units;
 
-//     axios.get(url)
-//         .then(res => {
-//             const temp = res.data.main.temp;
-//             const hum = res.data.main.humidity;
-//             const status = Object.assign({}, ...res.data.weather);
+    fetch(url)
+        .then((res)=> res.json())
+        .then(data => {
+            console.log(data);
+            const temp = data.main.temp;
+            console.log(temp);
+            const hum = data.main.humidity;
+            console.log(hum);
+            const pres = data.main.pressure;
+            const status = Object.assign({}, ...data.weather);
             
-//             cityName.textContent = res.data.name;
-//             weather.textContent = status.main;
-//             temperature.textContent = Math.floor(temp) + '°C';
-//             humidity.textContent = hum + '%';
+            cityName.textContent = data.name;
+            pressure.textContent = pres + 'hPa';
+            temperature.textContent = Math.floor(temp) + '°C';
+            humidity.textContent = hum + '%';
 
-//             warning.textContent = '';
-//             input.value = '';
+            warning.textContent = '';
+            input.value = '';
 
-//             // if (status.id >= 200 && status.id < 300) {
-//             //     photo.setAttribute('src', "../img/thunderstorm.png")
-//             // } else if (status.id >= 300 && status.id < 400) {
-//             //     photo.setAttribute('src', "../img/drizzle.png")
-//             // } else if (status.id >= 500 && status.id < 600) {
-//             //     photo.setAttribute('src', "../img/rain.png")
-//             // } else if  (status.id >= 600 && status.id < 700) {
-//             //     photo.setAttribute('src', "../img/ice.png")
-//             // } else if (status.id >= 700 && status.id < 799) {
-//             //     photo.setAttribute('src', "../img/fog.png")
-//             // } else if (status.id==800) {
-//             //     photo.setAttribute('src', "../img/sun.png")
-//             // } else if (status.id > 800 && status.id < 900) {
-//             //     photo.setAttribute('src', "../img/cloud.png")
-//             // } else {
-//             //     photo.setAttribute('src', "../img/unknown.png")
-//             // }
-//         })
-//         .catch(()=>{warning.textContent ='Wpisz poprawną nazwę miasta'})
-//     };
+            if (status.id >= 200 && status.id < 300) {
+                photo.setAttribute('src', "../src/assets/img/thunderstorm.png")
+            } else if (status.id >= 300 && status.id < 400) {
+                photo.setAttribute('src', "../src/assets/img/drizzle.png")
+            } else if (status.id >= 500 && status.id < 600) {
+                photo.setAttribute('src', "../src/assets/img/rain.png")
+            } else if  (status.id >= 600 && status.id < 700) {
+                photo.setAttribute('src', "../src/assets/img/ice.png")
+            } else if (status.id >= 700 && status.id < 799) {
+                photo.setAttribute('src', "../src/assets/img/fog.png")
+            } else if (status.id==800) {
+                photo.setAttribute('src', "../src/assets/img/sun.png")
+            } else if (status.id > 800 && status.id < 900) {
+                photo.setAttribute('src', "../src/assets/img/cloud.png")
+            } else {
+                photo.setAttribute('src', "../src/assets/img/unknown.png")
+            }
+        })
+        .catch(()=>{warning.textContent ='Wpisz poprawną nazwę miasta'})
+    };
 
-// const enterCheck = () => {
-//     if (event.keyCode===13) {
-//         getWeather();
-//     }
-// }
-// getWeather();
-// input.addEventListener('keyup', enterCheck)
-// btn.addEventListener('click', getWeather);
+const enterCheck = () => {
+    if (event.keyCode===13) {
+        getWeather();
+    }
+}
+getWeather();
+input.addEventListener('keyup', enterCheck)
+btn.addEventListener('click', getWeather);
 
 // Year footer
 let footer = document.querySelector('.footer');
